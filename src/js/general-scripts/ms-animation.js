f132576b9b6ddf42c5d2e1fc007cc3b7d592712e
Philipp Bulyha <dateofsciense@yandex.ru>
2025-01-07 02:50:41 +0300

    finish home page layout

---
+++ src/js/general-scripts/ms-animation.js
@@ -0,0 +1,29 @@
+const opacityMainAnimation = (opacityElement, opacityClass) => {
+    [...opacityElement].forEach(opacityElementSingle => {
+        opacityElementSingle.getBoundingClientRect().top < window.innerHeight - 250
+            ? opacityElementSingle.classList.add(opacityClass) : null;
+    })
+};
+
+const opacityAnimation = () => {
+    const opacityElement = document.querySelectorAll(`.animationOpacityBlock`);
+    opacityElement.length > 0 ? opacityMainAnimation(opacityElement, "animationOpacity") : null;
+};
+
+const leftAnimation = () => {
+    const opacityElement = document.querySelectorAll(`.animationLeftBlock`);
+    opacityElement.length > 0 ? opacityMainAnimation(opacityElement, "animationLeft") : null;
+};
+
+const rightAnimation = () => {
+    const opacityElement = document.querySelectorAll(`.animationRightBlock`);
+    opacityElement.length > 0 ? opacityMainAnimation(opacityElement, "animationRight") : null;
+};
+
+opacityAnimation();
+leftAnimation();
+rightAnimation();
+
+window.addEventListener("scroll", opacityAnimation);
+window.addEventListener("scroll", leftAnimation);
+window.addEventListener("scroll", rightAnimation);
--- src/js/ms-scripts.js
+++ src/js/ms-scripts.js
@@ -1,2 +1,3 @@
 import('./general-scripts/ms-dropdown.js')
 import('./general-scripts/ms-mobile-menu.js')
+import('./general-scripts/ms-animation.js')
